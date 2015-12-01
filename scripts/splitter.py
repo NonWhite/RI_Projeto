ead7326f4962c14244551338ad1920f7010e3e6c
Walter Perez <wperezurcia@gmail.com>
2015-12-01 13:52:38 -0200

    added some fields not allowed

--- scripts/splitter.py
+++ scripts/splitter.py
@@ -4,10 +4,12 @@ from time import strftime
 from copy import deepcopy as copy
 import xml.etree.ElementTree as ET
 
-not_allowed_fields = [ 'UNKNOWN' , 'DATELINE' ]
+not_allowed_fields = [ 'UNKNOWN' , 'DATELINE' , 'MKNOTE' ]
 mapping = {
 	'TITLE' : 'titulo' ,
-	'BODY' : 'content'
+	'BODY' : 'content' ,
+	'AUTHOR' : 'autor' ,
+	'TEXT' : 'content'
 }
 
 def is_start( line ) :
@@ -28,22 +30,22 @@ def parse( data , num_doc ) :
 			if len( children ) > 0 :
 				for ch in element :
 					if ch.tag == 'D' :
-						#parsed_data.append( ( m( tag ) , ch.text.strip() ) )
 						add( tag , ch.text )
 					else :
-						#parsed_data.append( ( m( ch.tag ) , ch.text.strip() ) )
 						add( ch.tag , ch.text )
 			else :
 				if element.text :
-					#parsed_data.append( ( m( tag ) , element.text.strip() ) )
 					add( tag , element.text )
 		parsed_data.append( ( 'id' , "reuters-%s" % ( "%s" % num_doc ).zfill( 5 ) ) )
+		parsed_data = [ ( k , r ) for ( k , r ) in parsed_data if k not in not_allowed_fields ]
+		'''
 		for ( k , r ) in parsed_data :
 			if k in not_allowed_fields :
 				parsed_data.remove( ( k , r ) )
 			if k == 'DATE' :
 				parsed_data.remove( ( k , r ) )
 				add( k , r[ :-3 ] )
+		'''
 	except Exception as e :
 		print e
 	return parsed_data
